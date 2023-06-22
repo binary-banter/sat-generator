337e03789fbcccbb7e8e8014997f45117a4752e3
jonathan <jonathantbrouwer@gmail.com>
2023-06-22 21:46:41 +0200

    Add cnf

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use minisat::Bool;
+use std::fs::File;
+use std::fmt::{Display, Formatter};
+use std::io::Write;
 
 const INPUT_COUNT: usize = 9;
-const INSTRUCTION_COUNT: usize = 3;
+const INSTRUCTION_COUNT: usize = 1;
 
 fn target_tt() -> Vec<bool> {
     to_truth_table::<9>(target)
@@ -20,8 +22,58 @@ fn to_truth_table<const N: usize>(f: impl Fn(u32) -> bool) -> Vec<bool> {
     array
 }
 
+struct Sat {
+    // Nodes indexed from 1 ..= node_count
+    node_count: isize,
+    clauses: Vec<Vec<isize>>
+}
+
+impl Sat {
+    fn new() -> Self {
+        Self {
+            node_count: 2,
+            clauses: vec![
+                vec![1],
+                vec![-2]
+            ]
+        }
+    }
+
+    fn new_lit(&mut self) -> isize {
+        self.node_count += 1;
+        self.node_count
+    }
+
+    fn get_const(&self, b: bool) -> isize {
+        if b { 1 } else { 2 }
+    }
+
+    fn add_clause(&mut self, c: impl IntoIterator<Item=isize>) {
+        self.clauses.push(c.into_iter().collect())
+    }
+}
+
+impl Display for Sat {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "p cnf {} {}", self.node_count, self.clauses.len())?;
+
+        for clause in &self.clauses {
+            for thing in clause{
+                write!(f, "{thing} ")?;
+            }
+
+            writeln!(f, "0")?;
+        }
+
+        Ok(())
+    }
+}
+
+
 fn main() {
-    let mut sat = minisat::Solver::new();
+    let mut f = File::create("game_of_life.cnf").unwrap();
+
+    let mut sat = Sat::new();
 
     let target_tt = target_tt();
 
@@ -45,20 +97,24 @@ fn main() {
             for _ in 0..i {
                 connections[i][side].push(sat.new_lit());
             }
-            sat.assert_exactly_one(connections[i][side].iter().cloned());
+            sat.add_clause(connections[i][side].iter().cloned());
         }
     }
 
     for input in 0..(1 << INPUT_COUNT) {
         let mut inputs = vec![];
         for i in 0..INPUT_COUNT {
-            inputs.push(Bool::Const(input & (1 << i) != 0));
+            inputs.push(sat.get_const(input & (1 << i) != 0));
         }
 
-        let mut outputs: Vec<Bool> = vec![];
-        for _ in 0..INSTRUCTION_COUNT {
+        let mut outputs: Vec<isize> = vec![];
+
+        for _ in 0..INSTRUCTION_COUNT - 1 {
             outputs.push(sat.new_lit());
         }
+        let output = target_tt[input];
+        outputs.push(sat.get_const(output));
+
         for i in 0..INSTRUCTION_COUNT {
             // Generate inputs
             let mut node_inputs = vec![];
@@ -67,20 +123,20 @@ fn main() {
                 let input_node = sat.new_lit();
                 for j in 0..INPUT_COUNT {
                     // connections[i][side][j] -> (inputs[j] = input_node)
-                    sat.add_clause([!connections[i][side][j], !inputs[j], input_node]);
-                    sat.add_clause([!connections[i][side][j], inputs[j], !input_node]);
+                    sat.add_clause([-connections[i][side][j], -inputs[j], input_node]);
+                    sat.add_clause([-connections[i][side][j], inputs[j], -input_node]);
                 }
                 for j in 0..i {
                     // connections[i][side][INPUT_COUNT+j] -> (outputs[j] = input_node)
                     sat.add_clause([
-                        !connections[i][side][INPUT_COUNT + j],
-                        !outputs[j],
+                        -connections[i][side][INPUT_COUNT + j],
+                        -outputs[j],
                         input_node,
                     ]);
                     sat.add_clause([
-                        !connections[i][side][INPUT_COUNT + j],
+                        -connections[i][side][INPUT_COUNT + j],
                         outputs[j],
-                        !input_node,
+                        -input_node,
                     ]);
                 }
                 node_inputs.push(input_node);
@@ -93,70 +149,26 @@ fn main() {
             // ~A & ~B & ~C => (output = truth_tables[i][0])
             for j in 0..8 {
                 let i1 = if j & (1 << 0) != 0 {
-                    !node_inputs[0]
+                    -node_inputs[0]
                 } else {
                     node_inputs[0]
                 };
                 let i2 = if j & (1 << 1) != 0 {
-                    !node_inputs[1]
+                    -node_inputs[1]
                 } else {
                     node_inputs[1]
                 };
                 let i3 = if j & (1 << 2) != 0 {
-                    !node_inputs[2]
+                    -node_inputs[2]
                 } else {
                     node_inputs[2]
                 };
 
-                sat.add_clause([i1, i2, i3, output, !truth_tables[i][j]]);
-                sat.add_clause([i1, i2, i3, !output, truth_tables[i][j]]);
+                sat.add_clause([i1, i2, i3, output, -truth_tables[i][j]]);
+                sat.add_clause([i1, i2, i3, -output, truth_tables[i][j]]);
             }
-
-            // // Check that output is used
-            // if i != INSTRUCTION_COUNT - 1 {
-            //     sat.or_literal(
-            //         (i + 1..INSTRUCTION_COUNT)
-            //             .map(|j: usize| connections[j].iter().map(|e| e[i]))
-            //             .flatten(),
-            //     );
-            // }
         }
-
-        let output = target_tt[input];
-        sat.equal(outputs.last().unwrap(), &Bool::Const(output));
     }
 
-    println!("starting model with #   vars = {}", sat.num_vars());
-    println!("starting model with #clauses = {}", sat.num_clauses());
-
-    let model = match sat.solve() {
-        Ok(model) => model,
-        Err(_) => {
-            println!("UNSAT.");
-            return;
-        }
-    };
-
-    for i in 0..INSTRUCTION_COUNT {
-        print!("{:2} - ", i + INPUT_COUNT);
-        truth_tables[i]
-            .iter()
-            .map(|t| model.value(t))
-            .for_each(|b| {
-                print!("{}", b as u8);
-            });
-        print!(" ");
-        for c in 0..3 {
-            let connection = connections[i][c]
-                .iter()
-                .map(|t| model.value(t))
-                .enumerate()
-                .filter(|(_, b)| *b)
-                .next()
-                .unwrap()
-                .0;
-            print!("{:2} ", connection);
-        }
-        println!();
-    }
+    write!(f, "{}", sat).unwrap();
 }
