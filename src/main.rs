414f05a59538c7dc6118ba7307682355566ab848
jonathan <jonathantbrouwer@gmail.com>
2023-06-22 16:44:43 +0200

    Improve generation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use minisat::{Bool};
 
 const INPUT_COUNT: usize = 9;
-const INSTRUCTION_COUNT: usize = 4;
+const INSTRUCTION_COUNT: usize = 12;
 
 fn target_tt() -> Vec<bool> {
     to_truth_table::<9>(target)
@@ -55,7 +55,7 @@ fn main() {
             inputs.push(Bool::Const(input & (1 << i) != 0));
         }
 
-        let mut outputs = vec![];
+        let mut outputs: Vec<Bool> = vec![];
         for i in 0..INSTRUCTION_COUNT {
             // Generate inputs
             let mut node_inputs = vec![];
@@ -64,15 +64,13 @@ fn main() {
                 let input_node = sat.new_lit();
                 for j in 0..INPUT_COUNT {
                     // connections[i][side][j] -> (inputs[j] = input_node)
-                    let equiv = sat.equiv(inputs[j], input_node);
-                    let impli = sat.implies(connections[i][side][j], equiv);
-                    sat.equal(&impli, &Bool::Const(true));
+                    sat.add_clause([!connections[i][side][j], !inputs[j], input_node]);
+                    sat.add_clause([!connections[i][side][j], inputs[j], !input_node]);
                 }
                 for j in 0..i {
                     // connections[i][side][INPUT_COUNT+j] -> (outputs[j] = input_node)
-                    let equiv = sat.equiv(outputs[j], input_node);
-                    let impli = sat.implies(connections[i][side][INPUT_COUNT+j], equiv);
-                    sat.equal(&impli, &Bool::Const(true));
+                    sat.add_clause([!connections[i][side][INPUT_COUNT+j], !outputs[j], input_node]);
+                    sat.add_clause([!connections[i][side][INPUT_COUNT+j], outputs[j], !input_node]);
                 }
                 node_inputs.push(input_node);
             }
@@ -82,14 +80,12 @@ fn main() {
 
             // ~A & ~B & ~C => (output = truth_tables[i][0])
             for j in 0..8 {
-                let and = sat.and_literal([
-                    if j & (1 << 0) == 0 { !node_inputs[0] } else { node_inputs[0] },
-                    if j & (1 << 1) == 0 { !node_inputs[1] } else { node_inputs[1] },
-                    if j & (1 << 2) == 0 { !node_inputs[2] } else { node_inputs[2] },
-                ]);
-                let equiv = sat.equiv(output, truth_tables[i][j]);
-                let impli = sat.implies(and, equiv);
-                sat.equal(&impli, &Bool::Const(true));
+                let i1 = if j & (1 << 0) != 0 { !node_inputs[0] } else { node_inputs[0] };
+                let i2 = if j & (1 << 1) != 0 { !node_inputs[1] } else { node_inputs[1] };
+                let i3 = if j & (1 << 2) != 0 { !node_inputs[2] } else { node_inputs[2] };
+
+                sat.add_clause([i1, i2, i3, output, !truth_tables[i][j]]);
+                sat.add_clause([i1, i2, i3, !output, truth_tables[i][j]]);
             }
 
             outputs.push(output);
